1de2fa16df717e445f3da3a1c9da45f8025923e6
Gruyere <united.states.erland@gmail.com>
2026-05-02 23:11:59 +0200

    Updated cog.py

--- leaderboard/leaderboard/cog.py
+++ leaderboard/leaderboard/cog.py
@@ -2,10 +2,14 @@ import discord
 from discord import app_commands, Embed, Color
 from discord.ext import commands
 from bd_models.models import Player
-from django.db.models import Count
+from django.db.models import Count, Q
 from ballsdex.core.bot import BallsDexBot
 from ballsdex.settings import settings
 from asgiref.sync import sync_to_async
+from ballsdex.core.utils.transformers import (
+    BallEnabledTransform,
+)
+
 
 class Leaderboard(commands.Cog):
     """
@@ -15,12 +19,12 @@ class Leaderboard(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @app_commands.command(name="leaderboard", description=f"Show the top players of {settings.bot_name}!")
-    async def leaderboard(self, interaction: discord.Interaction["BallsDexBot"], economy: bool = False):
+    @app_commands.command(name="leaderboard", description=f"Shows the top players of {settings.bot_name}!")
+    async def leaderboard(self, interaction: discord.Interaction["BallsDexBot"], economy: bool = False, ephemeral: bool = False, amount: int = 10, collectible: BallEnabledTransform | None = None,):
         """
         Displays the most addicted i mean best players of this dex
         """
-        await interaction.response.defer(ephemeral=True, thinking=True)
+        await interaction.response.defer(ephemeral=ephemeral, thinking=True)
 
         if economy:
             if not settings.currency_name:
@@ -28,45 +32,61 @@ class Leaderboard(commands.Cog):
                 return
         
         if not economy:
-            players = await sync_to_async(
-                lambda: list(
-                    Player.objects.annotate(ball_count=Count("balls")).order_by("-ball_count")[:10]
-                )
-            )()
+            if not collectible:
+                players = await sync_to_async(
+                    lambda: list(
+                        Player.objects.annotate(ball_count=Count("balls")).order_by("-ball_count")[:amount]
+                    )
+                )()
+            else:
+                players = await sync_to_async(
+                    lambda: list(
+                        Player.objects.annotate(ball_count=Count("balls", filter=Q(balls__ball=collectible))).order_by("-ball_count")[:amount]
+                    )
+                )()  
         else:
             players = await sync_to_async(
                 lambda: list(
-                    Player.objects.order_by("-money")[:10]
+                    Player.objects.order_by("-money")[:amount]
                 )
             )()
 
         if not players:
             await interaction.followup.send("No players found.", ephemeral=True)
             return
-
-        embed = discord.Embed(
-            title=f"Top 10 players of {settings.bot_name}",
-            color=discord.Color.gold()
-        )
-
-        if economy == False:
-            text = ""
-            for i, player in enumerate(players, start=1):
-                user = self.bot.get_user(player.discord_id) or await self.bot.fetch_user(player.discord_id)
-                text += f"**{i}. {user.name}** — {settings.plural_collectible_name}: {player.ball_count}\n"
+        if not collectible:
+            embed = discord.Embed(
+                title=f"Top {amount} players of {settings.bot_name}",
+                color=discord.Color.gold()
+            )
         else:
-            text = ""
+            embed = discord.Embed(
+                title=f"Top {amount} players that have {collectible}",
+                color=discord.Color.gold()
+            )
+        text = ""
+        if economy == False:
+            if not collectible:
+                for i, player in enumerate(players, start=1):
+                    user = self.bot.get_user(player.discord_id) or await self.bot.fetch_user(player.discord_id)
+                    text += f"**{i}. {user.name}** {settings.plural_collectible_name}: {player.ball_count}\n"
+            else:
+                for i, player in enumerate(players, start=1):
+                    user = self.bot.get_user(player.discord_id) or await self.bot.fetch_user(player.discord_id)
+                    text += f"**{i}. {user.name}** {collectible}(s): {player.ball_count}\n"
+        else:
             for i, player in enumerate(players, start=1):
                 user = self.bot.get_user(player.discord_id) or await self.bot.fetch_user(player.discord_id)
                 if settings.currency_symbol_before == True:
-                    text += f"**{i}. {user.name}** — {settings.currency_symbol}{player.money}\n"
+                    text += f"**{i}. {user.name}** {settings.currency_symbol}{player.money}\n"
                 else:
-                    text += f"**{i}. {user.name}** — {player.money}{settings.currency_symbol}\n"
+                    text += f"**{i}. {user.name}** {player.money}{settings.currency_symbol}\n"
 
         embed.description = text
         embed.set_footer(text="Made by @unitedstatesoferland")
         embed.set_thumbnail(url=interaction.user.display_avatar.url)
 
         await interaction.followup.send(embed=embed)
+
 
 
